9256fd45944ca30ce8574d4705230fb86e4b2c63
L2c <kuba.brambor@gmail.com>
2019-08-31 13:53:25 +0200

    zadania z obiektow

---
+++ zadaniaObiekty/zadanie1.js
@@ -0,0 +1,28 @@
+var Car = function(brand, color, year, distance, oilNum){
+    this.brand = brand;
+    this.color = color;
+    this.year = year;
+    this.distance = distance;
+    this.oilNum = oilNum;
+}
+
+Car.prototype.drive = function(kilometers){
+    this.distance = this.distance + kilometers
+    console.log(this.distance);
+    this.oilNum = this.oilNum + kilometers;
+    if (this.oilNum > 10000){
+        console.log("Nalezy wymienic olej");
+    }
+}
+
+Car.prototype.colore = function(color){
+    this.color = color;
+}
+
+Car.prototype.message = function(){
+    console.log(`Przebieg dla samochodu o marce ${this.brand} wynosi ${this.distance}`);
+}
+
+var ford = new Car("Ford", "red", "1998", 120000, 0);
+var vw = new Car("VW", "yellow", "2003", 292000, 0);
+var mazda = new Car("Mazda", "green", "2010", 92000, 0);
